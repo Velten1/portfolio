dda48a7407e8a461eeb6afa9bdb6b654a03742ea
Velten <caioryan.ca@gmail.com>
2025-12-10 23:31:31 -0300

    Initial commit - Setup Next.js 14 com TypeScript

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  images: {
+    domains: ['via.placeholder.com', 'placehold.co'],
+  },
+}
+
+module.exports = nextConfig
+
